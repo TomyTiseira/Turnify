1327de968bcfb8c09ecf64f84e30718e0cc80bdc
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-03-11 00:10:51 -0300

    Fix user already exists exception status code to conflict

--- users/src/users/domain/exceptions/user-already-exists.exception.ts
+++ users/src/users/domain/exceptions/user-already-exists.exception.ts
@@ -4,7 +4,7 @@ import { RpcException } from '@nestjs/microservices';
 export class UserAlreadyExistsException extends RpcException {
   constructor(email: string) {
     super({
-      status: HttpStatus.BAD_REQUEST,
+      status: HttpStatus.CONFLICT,
       message: `El usuario con el email ${email} ya existe.`,
     });
     this.name = 'UserAlreadyExistsException';
